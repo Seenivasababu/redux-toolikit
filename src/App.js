c01ac2739a4b63e39c9f1d339cae9db9a41b5b8c
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-09-10 22:50:13 +0530

    Learned React Reduc toolkit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addSong,removeSong } from './store';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => {
+    return state.songs;
+  });
+  const rand = Math.floor(Math.random() * 1000);
+
+  const handleSubmit = () => {
+    dispatch(addSong(`${rand}`));
+  };
+
+  const hadleDelete = (song) => {
+   dispatch(removeSong(song))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleSubmit}> Add songs</button>
+      <br />
+      <br />
+
+      {data.map((item) => {
+        return (
+          <div key={item}>
+            <li>
+              
+              {item}
+               <button onClick={()=>hadleDelete(item)}>X</button>
+            </li>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
